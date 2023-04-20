0994448fd5801d59245883c42b94f46ea9c979b4
Rachana priyadarshni samal <rachanapriyadarshnisamal@gmail.com>
2023-04-20 18:10:16 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,57 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import Laravel from "./tech/Laravel.png";
+import php from "./tech/php.png";
+import vuejs from "./tech/vuejs.png";
+
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+    logo,
+    backend,
+    creator,
+    mobile,
+    web,
+    github,
+    menu,
+    close,
+    css,
+    git,
+    html,
+    javascript,
+    php,
+    reactjs,
+    Laravel,
+    tailwind,
+    vuejs,
+    threejs,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    carrent,
+    jobit,
+    tripguide,
+};
